9c46f11a945b70ed925c6a8649674af734ba5cbf
muerwre <gotham48@gmail.com>
2019-02-18 12:21:29 +0700

    gpx fix

--- src/utils/gpx.ts
+++ src/utils/gpx.ts
@@ -9,36 +9,34 @@ interface IGetGPXString {
   points: Array<IRoutePoint>,
   title?: string,
 }
-
-export const getGPXString = ({ points, title }: IGetGPXString): string => (`
-  <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-    <gpx>
-      <rte>
-        <name>${title || 'GPX Track'}</name>
-        ${
-          points.reduce((cat, { lat, lng }, index) => (
-              ` ${cat}
-                <wpt lat="${lat.toFixed(6)}" lon="${lng.toFixed(6)}"></wpt>`
-          ), '')
-        }        
-      </rte>
-    </gpx>
-`);
-
-// export const getGPXString = ({ points, title }: IGetGPXString): string => (`
-//   <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+//
+// export const getGPXString = ({ points, title }: IGetGPXString): string => (`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 //     <gpx>
 //       <rte>
 //         <name>${title || 'GPX Track'}</name>
 //         ${
 //           points.reduce((cat, { lat, lng }, index) => (
-//               `${cat}
-// <rtept lat="${lat}" lon="${lng}"></rtept>`
+//               ` ${cat}
+//                 <wpt lat="${lat.toFixed(6)}" lon="${lng.toFixed(6)}"></wpt>`
 //           ), '')
 //         }
 //       </rte>
 //     </gpx>
 // `);
+
+export const getGPXString = ({ points, title }: IGetGPXString): string => (`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+    <gpx>
+      <rte>
+        <name>${title || 'GPX Track'}</name>
+        ${
+          points.reduce((cat, { lat, lng }, index) => (
+              `${cat}
+<rtept lat="${lat}" lon="${lng}"></rtept>`
+          ), '')
+        }
+      </rte>
+    </gpx>
+`);
 
 export const downloadGPXTrack = ({ track, title }: { track: string, title?: string }) => (
   saveAs(
