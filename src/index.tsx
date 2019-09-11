350a090ac3494c0bbf98fc75ff6d5e76a9d5055f
Integral Team <repo@sfxdx.ru>
2019-09-11 17:16:31 +0700

    added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -100,3 +100,11 @@ export const Index = () => (
 );
 
 ReactDOM.render(<Index />, document.getElementById('index'));
+
+(function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
+      .then(() => console.log('Service Worker registered successfully.'))
+      .catch(error => console.log('Service Worker registration failed:', error));
+  }
+}());
