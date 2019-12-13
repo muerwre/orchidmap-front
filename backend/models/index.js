dd6f16379346cfc8ab01507356139251dfd6e2e6
Fedor Katurov <gotham48@gmail.com>
2019-12-13 13:23:54 +0700

    removed backend part

--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const { UserSchema } = require('./User');
-const { RouteSchema } = require('./Route');
-
-module.exports.User = mongoose.model('User', UserSchema);
-module.exports.Route = mongoose.model('Route', RouteSchema);
-
--- backend/routes/auth/list.js
+++
@@ -1,9 +0,0 @@
-const { User } = require('../../models');
-
-module.exports = (req, res) => User.find((err, articles) => {
-  if (!err) return res.send(articles);
-
-  res.statusCode = 500;
-  return res.send({ error: 'Server error' });
-});
-
