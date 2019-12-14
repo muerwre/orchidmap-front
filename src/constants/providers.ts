6880d155ed35e7aad239b7b5d176d7e7c509baef
Fedor Katurov <gotham48@gmail.com>
2019-12-14 19:32:00 +0700

    removed providers with cached ones

--- src/constants/providers.ts
+++ src/constants/providers.ts
@@ -4,33 +4,35 @@ export interface IProvider {
   range: Array<string | number>,
 }
 
-export interface ITileMaps {
-  WATERCOLOR: IProvider,
-  DGIS: IProvider,
-  DEFAULT: IProvider,
-  DARQ: IProvider,
-  BLANK: IProvider,
-  HOT: IProvider,
-  YSAT: IProvider,
-  YMAP: IProvider,
-  SAT: IProvider,
-  ESAT: IProvider,
+export type ITileMaps = Record<string, IProvider>
+//  {  
+  // WATERCOLOR: IProvider,
+  // DGIS: IProvider,
+  // DEFAULT: IProvider,
+  // DARQ: IProvider,
+  // BLANK: IProvider,
+  // HOT: IProvider,
+  // YSAT: IProvider,
+  // YMAP: IProvider,
+  // SAT: IProvider,
+  // ESAT: IProvider,
   // CACHE_OSM: IProvider,
   // CACHE_CARTO: IProvider,
-}
+// }
 
 
 // Стили карт
 const TILEMAPS: ITileMaps = {
-  WATERCOLOR: {
-    name: 'Watercolor',
-    url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg',
-    range: [1, 2, 3, 4],
-  },
+  // WATERCOLOR: {
+    // name: 'Watercolor',
+    // url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg',
+    // range: [1, 2, 3, 4],
+  // },
   DGIS: {
     name: '2gis',
-    url: 'https://tile1.maps.2gis.com/tiles?x={x}&y={y}&z={z}&v=1',
-    range: [1, 2, 3],
+    // url: 'https://tile1.maps.2gis.com/tiles?x={x}&y={y}&z={z}&v=1',
+    url: 'https://{s}.dgis.tile.vault48.org/tiles?x={x}&y={y}&z={z}&v=1',
+    range: ['a','b','c'],
   },
   DEFAULT: {
     name: 'OpenStreetMap',
@@ -38,41 +40,45 @@ const TILEMAPS: ITileMaps = {
     url: 'https://{s}.osm.tile.vault48.org/{z}/{x}/{y}.png',
     range: ['a', 'b', 'c'],
   },
-  DARQ: {
-    name: 'Darq',
-    url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-    range: [1, 2, 3, 4],
-  },
+  // DARQ: {
+    // name: 'Darq',
+    // url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+    // range: [1, 2, 3, 4],
+  // },
   BLANK: {
     name: 'Blanque',
     // url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
     url: 'https://{s}.carto.tile.vault48.org/light_all/{z}/{x}/{y}.png',
-    range: [1, 2, 3, 4],
+    // range: [1, 2, 3, 4],
+    range: ['a','b','c'],
   },
   HOT: {
     name: 'Hot',
-    url: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+    // url: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+    url: 'https://{s}.hot.tile.vault48.org/hot/{z}/{x}/{y}.png',
     range: ['a', 'b', 'c'],
   },
-  SAT: {
-    name: 'Google Sattelite',
-    url: 'https://mt{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
-    range: [0, 1, 2, 3],
-  },
-  YMAP: {
-    name: 'Yandex',
-    url: 'https://vec0{s}.maps.yandex.net/tiles?l=map&v=17.04.16-0&x={x}&y={y}&z={z}&scale=1&lang=ru_RU',
-    range: [1, 2, 3, 4],
-  },
-  YSAT: {
-    name: 'YandexSat',
-    url: 'https://sat0{s}.maps.yandex.net/tiles?l=sat&v=3.330.0&x={x}&y={y}&z={z}&lang=ru_RU',
-    range: [1, 2, 3, 4],
-  },
+  // SAT: {
+    // name: 'Google Sattelite',
+    // url: 'https://mt{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+    // range: [0, 1, 2, 3],
+  // },
+  // YMAP: {
+    // name: 'Yandex',
+    // url: 'https://vec0{s}.maps.yandex.net/tiles?l=map&v=17.04.16-0&x={x}&y={y}&z={z}&scale=1&lang=ru_RU',
+    // range: [1, 2, 3, 4],
+  // },
+  // YSAT: {
+    // name: 'YandexSat',
+    // url: 'https://sat0{s}.maps.yandex.net/tiles?l=sat&v=3.330.0&x={x}&y={y}&z={z}&lang=ru_RU',
+    // range: [1, 2, 3, 4],
+  // },
   ESAT: {
     name: 'Sattelite',
-    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    range: [1,2,3,4],
+    // url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    url: 'https://{s}.esat.tile.vault48.org/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    // range: [1,2,3,4],
+    range: ['a','b','c'],
   }
 };
 
