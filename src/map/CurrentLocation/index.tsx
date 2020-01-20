5ef427cb4551ab4c7eced6e9e2948b6bd7efa625
Fedor Katurov <gotham48@gmail.com>
2020-01-20 12:29:49 +0700

    zooming on marker click

--- src/map/CurrentLocation/index.tsx
+++ src/map/CurrentLocation/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useCallback } from 'react';
 import { LatLngLiteral, marker, Marker, DivIcon } from 'leaflet';
 import { MainMap } from '~/constants/map';
 
@@ -7,6 +7,8 @@ interface IProps {
 }
 
 const CurrentLocation: FC<IProps> = ({ location }) => {
+  const zoomToLocation = useCallback(({ latlng }) => MainMap.setView(latlng, 17), [MainMap]);
+
   useEffect(() => {
     if (!location) return;
 
@@ -15,17 +17,21 @@ const CurrentLocation: FC<IProps> = ({ location }) => {
         html: `
           <div class="current-location">
             <svg width="28" height="28" viewBox="0 0 20 20">
-              <circle r="1.846" cy="1.846" cx="5.088"/>
-              <path d="M3.004 4.326h4l2-3 1 1-3 4v10h-1l-1-7-1 7h-1v-10s-3.125-4-3-4l1-1z"/>
-              <ellipse ry="1" rx="4" cy="16.326" cx="5.004" opacity=".262" fill="black" />    
+              <g transform="translate(5 0)">
+                <circle r="1.846" cy="1.846" cx="5.088"/>
+                <path d="M3.004 4.326h4l2-3 1 1-3 4v10h-1l-1-7-1 7h-1v-10s-3.125-4-3-4l1-1z"/>
+                <ellipse ry="1" rx="4" cy="16.326" cx="5.004" opacity=".262" fill="black" />    
+              </g>
             </div>
           </div>
         `,
       }),
-    }).addTo(MainMap);
+    })
+      .on('click', zoomToLocation)
+      .addTo(MainMap);
 
     return () => item.removeFrom(MainMap);
-  }, [MainMap, location]);
+  }, [MainMap, location, zoomToLocation]);
 
   return null;
 };
