b2357cf52218811bd80305ea348c0153062e7be7
Fedor Katurov <gotham48@gmail.com>
2020-01-27 15:51:29 +0700

    disabled append features for tracks

--- src/components/gpx/GpxDialogRow.tsx
+++ src/components/gpx/GpxDialogRow.tsx
@@ -6,28 +6,48 @@ import classnames from 'classnames';
 
 interface IProps {
   item: IGpxRoute;
-  index: number
+  index: number;
   enabled: boolean;
 
-  onFocusRoute: (i: number) => void
-  onRouteDrop: (i: number) => void
-  onRouteToggle: (i: number) => void
-  onRouteColor: (i: number) => void
+  onFocusRoute: (i: number) => void;
+  onRouteDrop: (i: number) => void;
+  onRouteToggle: (i: number) => void;
+  onRouteColor: (i: number) => void;
 }
 
-const GpxDialogRow: FC<IProps> = ({ item, index, enabled, onRouteToggle, onFocusRoute, onRouteDrop, onRouteColor }) => {
+const GpxDialogRow: FC<IProps> = ({
+  item,
+  index,
+  enabled,
+  onRouteToggle,
+  onFocusRoute,
+  onRouteDrop,
+  onRouteColor,
+}) => {
   return (
-    <div className={classnames("gpx-row", { 'gpx-row_disabled': !enabled || !item.enabled })}>
-      <div className="gpx-row__color" style={{ backgroundColor: item.color }} onClick={() => onRouteColor(index)}/>
+    <div className={classnames('gpx-row', { 'gpx-row_disabled': !enabled || !item.enabled })}>
+      <div
+        className="gpx-row__color"
+        style={{ backgroundColor: item.color }}
+        onClick={() => onRouteColor(index)}
+      />
 
       <div className="gpx-row__title" onClick={() => onFocusRoute(index)}>
         {item.name}
       </div>
 
       <div className="gpx-row__buttons">
-        <div onClick={() => onRouteDrop(index)}><Icon icon="icon-to-poly" size={24} /></div>
-        <div onClick={() => onRouteDrop(index)}><Icon icon="icon-trash-6" size={24} /></div>
-        <div><Switch active={item.enabled} onPress={() => onRouteToggle(index)}/></div>
+        {false && (
+          <div onClick={() => onRouteDrop(index)}>
+            <Icon icon="icon-to-poly" size={24} />
+          </div>
+        )}
+        <div onClick={() => onRouteDrop(index)}>
+          <Icon icon="icon-trash-6" size={24} />
+        </div>
+        <div>
+          <Switch active={item.enabled} onPress={() => onRouteToggle(index)} />
+        </div>
       </div>
     </div>
   );
