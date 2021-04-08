82cd56fee1b5f3b6716eda4956d7502d6e25dc46
Fedor Katurov <gotham48@gmail.com>
2021-04-08 11:25:41 +0700

    added configs

---
+++ src/config/frontend.ts
@@ -0,0 +1,29 @@
+import { DEFAULT_PROVIDER, PROVIDERS } from '~/constants/providers';
+import { LatLngLiteral } from 'leaflet';
+
+const PUBLIC_PATH = process.env.REACT_APP_PUBLIC_PATH || '';
+const API_ADDR = process.env.REACT_APP_API_ADDR || '';
+const OSRM_URL = process.env.REACT_APP_OSRM_URL || '';
+const OSRM_PROFILE = process.env.REACT_APP_OSRM_PROFILE || 'bike';
+const OSRM_TEST_URL = ([south_west, north_east]: LatLngLiteral[]) => (
+  `${OSRM_URL}/${OSRM_PROFILE}/${Object.values(south_west).join(',')};${Object.values(north_east).join(',')}`
+);
+
+export const CLIENT = {
+  OSRM_URL,
+  API_ADDR,
+  OSRM_TEST_URL,
+  OSRM_PROFILE,
+  STROKE_WIDTH: 5,
+  PUBLIC_PATH,
+  NOMINATIM_TEST_URL: '',
+  NOMINATIM_URL: '',
+};
+
+export const COLORS = {
+  PATH_COLOR: ['#ff7700', '#ff3344'],
+};
+
+export const PROVIDER = PROVIDERS[DEFAULT_PROVIDER];
+
+export const MOBILE_BREAKPOINT = 768;
---
+++ src/config/pwa.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  MANIFEST: (src) => ({
+    name: 'Редактор маршрутов',
+    short_name: 'Маршруты',
+    description: 'Велосипедные маршруты в новосибирске',
+    background_color: '#333333',
+    theme_color: '#01579b',
+    display: 'fullscreen',
+    'theme-color': '#01579b',
+    start_url: '/',
+    icons: [
+      {
+        src, // : path.resolve('./src/sprites/app.png')
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons')
+      }
+    ]
+  }),
+  PUBLIC_PATH: process.env.REACT_APP_PUBLIC_PATH,
+};
